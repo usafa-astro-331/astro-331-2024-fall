dbbae0212c29bdf248b0451d216978cd123a85c7
jordanfirthussf <jordan.firth@afacademy.af.edu>
2024-07-03 09:30:25 -0600

    initial lab 1

---
+++ labs/01_electrical_power/INA219_pins.h
@@ -0,0 +1,18 @@
+// INA219 sensor connects with I2C
+// INA219 pin = Arduino pin
+//
+// if using Arduino MKR Zero
+// SDA is arduino pin 11
+// SCL is arduino pin 12
+//
+// if using Teensy 4.1
+// SDA is pin 18
+// SCL is pin 19
+//
+// if using QWIIC connector:
+// red = 3.3V
+// black = gnd
+// blue = SDA 
+// yellow = SCL 
+//
+// otherwise connect each pin manually
